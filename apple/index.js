aed75be86b26b1f3902c0776091a2c1e68b71653
marina wafik <152493070+marinawafik14@users.noreply.github.com>
2024-05-22 22:01:34 +0300

    Add files via upload

---
+++ apple/index.js
@@ -0,0 +1,10 @@
+const img = document.querySelector(".img");
+const container  = document.querySelector(".container")
+function phones(phone){
+img.src = phone
+
+
+}
+function colors(color) {
+    container.style.background= color
+}
